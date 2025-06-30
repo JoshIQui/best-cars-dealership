3db05f9eb0ba6cdb67b8a5f031c1354664eb04b9
JoshIQui <joshiqa01@gmail.com>
2025-06-30 17:45:40 -0400

    created frontend for cars

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -37,4 +37,5 @@ urlpatterns = [
         TemplateView.as_view(template_name="index.html"),
     ),
     path("", TemplateView.as_view(template_name="Home.html")),
+    path('searchcars/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
